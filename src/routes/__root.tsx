eb1426584015a43a466ff382ed23d409b5b30202
faizanahmad05 <fa7055726@gmail.com>
2026-07-18 22:35:00 +0500

    Update favicon link path to absolute URL

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -105,7 +105,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "src/images/logo.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/images/logo.ico", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
